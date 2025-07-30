38a60a62a011d3edbfa6f5aed721540e9f423431
detolove1006 <158586434+detolove1006@users.noreply.github.com>
2025-07-30 19:45:21 +0900

    2025 7.30 문자열

--- program/source.c
+++ program/source.c
@@ -33,8 +33,8 @@ void main()
 	// 변경할 수 없습니다.
 
 
-    //int array[] = { 10,20,30,40,50 };
-	
+	//int array[] = { 10,20,30,40,50 };
+	//
 	//int size = sizeof(array) / sizeof(array[0]);
 	//
 	//
@@ -56,7 +56,11 @@ void main()
 	//
 	//*pointer = 25;
 	//
-	//printf("변수의 값 : %d\n", array[1]); 
+	// printf("pointer 변수의 값 : %p\n", pointer);
+	// printf("array[%d]의 주소 : %p\n", 1, &array[1]);
+	// 
+	// printf("array[%d]의 값 : %d\n", 1, array[1]);
+
 
 
 	// 배열의 크기는 생햑할 수 있으며. 초기화 목록에서.
@@ -67,9 +71,70 @@ void main()
 	//배열의 시작 주소를 가리킵니다.
 
 
+#pragma region 문자열
+	// 연속적인 메모리 공간에 저장된 문자 변수의 집합입니다.
+
+	//const char* message = "packet";
+	//char title[] = { "league of legend" };
+	//
+	//
+	// //*message = 'e';
+	//
+	// printf("message가 가리키는 문자열이 뭘까요? : %s\n", message);
+	// printf("message[3]의 값 : %c\n", message[3]);
+	//
+	// message = "bit";
+	//
+	// //문자열의 경우 포인터를 이용하여 문자를 상수를 가리키도록 할 수
+	// //있으며, 문자에 상수는 데이터 영역의 읽기 전용 공간에
+	// // 저장되기 때문에 문자열의 값을 변경할 수 없습니다.
+	//
+	// printf("message의 주소 값: %p\n", message);
+	// printf("message가 가리키는 문자열이 뭘까요? : %s\n", message);
+	//
+	//
+	// // 문자열은 공백도 함께 메모리 공간에 포함되며, 마지막 메모리
+	// // 공간의 요소에 문자열 끝을 알려주는 제어 문자가 추가됩니다.
+	//
+	// printf("타이틀의 값은 ? : %s\n", title);
+	//
+	//title[6] = '\0';
+	//
+	//printf("타이틀의 값은 ? : %s\n", title);
+
+	///// title[] = { "clash of clan" };
+
+	 //문자열의 경우 서로 연속적인 메모리 공간으로 연결되어 있지만.
+	 // 문자 배열 사이에 무효의 문자를 넣게 되면 무효의 문자까지만 출력됩니다.
+#pragma endregion
+
+#pragma region (2)차원 배열
+// 배열의 요소로 또 다른 배열을 가지는 배열입니다.
+
+
+int array2D[3][3] = 9
+{
+	for (int i = 0; i < 10; i++);
+
+
+	{1,2,3},
+	{4,5,6},
+	{7,8,9},
+
+};
+
+
+
+
+
+// 2차원 배열은 행과 열로 구분되며, 앞에 있는 배열은 
+// 행을 의미하고, 뒤에있는 배열은 열을 의미합니다.
+#pragma endregion
+
+
 #pragma endregion                                                               
 	
-//pull request
+
 
 	
 	
