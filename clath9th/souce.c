4010ade432c21dce2a20725b213ef23f5078591b
detolove1006 <158586434+detolove1006@users.noreply.github.com>
2025-07-31 19:46:13 +0900

    2025 7.31 배열

---
+++ clath9th/souce.c
@@ -0,0 +1,178 @@
+﻿#include <stdio.h>
+
+
+
+void main()
+{
+
+#pragma region 배열
+	// 같은 자료형의 변수들로 이루어진 유한 집합입니다.
+
+
+
+	//int a[5];
+	//
+	////배열의 경우 첫번쩨 원소는 0부터 시작합니다.
+	//
+	//
+	//for (int a1 = 0; a1 < 5; a1++)
+	//{
+	//
+	//	a[a1] = (a1 + 1) * 10;
+	//
+	//	printf("a[%d] = %d\n", a1, a[a1]);
+	//
+	//
+	//	
+	//		
+	//
+	//}
+
+
+	// 배열의 메모리 공간은 프로그램이 실행하는 동안 
+	// 변경할 수 없습니다.
+
+
+	//int array[] = { 10,20,30,40,50 };
+	//
+	//int size = sizeof(array) / sizeof(array[0]);
+	//
+	//
+	//for (int i = 0; i < size; i++)
+	//{
+	//	printf("array[%d]의 값 ; %d\n", i, array[i]);
+	//
+	//
+	//}
+	//printf(" 시작주소: %p\n", array);
+	//
+	//
+	//
+	//int* pointer = array;
+	//
+	//pointer = pointer + 1;
+	//
+	//
+	//
+	//*pointer = 25;
+	//
+	// printf("pointer 변수의 값 : %p\n", pointer);
+	// printf("array[%d]의 주소 : %p\n", 1, &array[1]);
+	// 
+	// printf("array[%d]의 값 : %d\n", 1, array[1]);
+
+
+
+	// 배열의 크기는 생햑할 수 있으며. 초기화 목록에서.
+	// 설정한 요소에 따라 배열의 크기 가 결정됩니다.
+
+
+	//배열은 연속적인 메모리 공간을 가지고 있으며. 배열의 이름은
+	//배열의 시작 주소를 가리킵니다.
+
+
+#pragma region 문자열
+	// 연속적인 메모리 공간에 저장된 문자 변수의 집합입니다.
+
+	//const char* message = "packet";
+	//char title[] = { "league of legend" };
+	//
+	//
+	// //*message = 'e';
+	//
+	// printf("message가 가리키는 문자열이 뭘까요? : %s\n", message);
+	// printf("message[3]의 값 : %c\n", message[3]);
+	//
+	// message = "bit";
+	//
+	// //문자열의 경우 포인터를 이용하여 문자를 상수를 가리키도록 할 수
+	// //있으며, 문자에 상수는 데이터 영역의 읽기 전용 공간에
+	// // 저장되기 때문에 문자열의 값을 변경할 수 없습니다.
+	//
+	// printf("message의 주소 값: %p\n", message);
+	// printf("message가 가리키는 문자열이 뭘까요? : %s\n", message);
+	//
+	//
+	// // 문자열은 공백도 함께 메모리 공간에 포함되며, 마지막 메모리
+	// // 공간의 요소에 문자열 끝을 알려주는 제어 문자가 추가됩니다.
+	//
+	// printf("타이틀의 값은 ? : %s\n", title);
+	//
+	//title[6] = '\0';
+	//
+	//printf("타이틀의 값은 ? : %s\n", title);
+
+	///// title[] = { "clash of clan" };
+
+	 //문자열의 경우 서로 연속적인 메모리 공간으로 연결되어 있지만.
+	 // 문자 배열 사이에 무효의 문자를 넣게 되면 무효의 문자까지만 출력됩니다.
+#pragma endregion
+
+#pragma region (2)차원 배열
+// 배열의 요소로 또 다른 배열을 가지는 배열입니다.
+
+
+//int array2D[3][3] =
+//{
+//
+//
+//	{ 1,2,3 },
+//	{ 4,5,6 },
+//	{ 7,8,9 },
+//};
+//
+//for(int i = 0; i < 3; i++ )
+//{ 
+//
+//	for (int a = 0; a < 3; a++)
+//	{
+//		printf("[%d]", array2D[i][a]); //(수정)
+//
+//	}
+//
+//	printf("\n");
+//}
+
+
+
+
+
+// 2차원 배열은 행과 열로 구분되며, 앞에 있는 배열은 
+// 행을 의미하고, 뒤에있는 배열은 열을 의미합니다.
+
+
+
+
+
+#pragma endregion
+
+#pragma region 에스키 코드
+// 미국 국립 표준 협회에서 표준화한 정보 교환용
+// / bit 부호 제계입니다.
+
+//	char alphabet = 'A';
+//
+//	printf("alphabet 변수의 값 : %d\n", alphabet);
+//
+//	for (int i = 0; i < 26; i++)
+//	{
+//		printf("%c", alphabet + i);
+//	}
+
+
+#pragma endregion
+
+
+
+
+
+
+
+
+
+#pragma endregion                                                               
+
+
+
+
+}
--- program/source.c
+++ program/source.c
@@ -1,156 +1,110 @@
 ﻿#include <stdio.h>
 
 
-
-void main()
-{
-
-#pragma region 배열
-	// 같은 자료형의 변수들로 이루어진 유한 집합입니다.
-
-
-
-	//int a[5];
-	//
-	////배열의 경우 첫번쩨 원소는 0부터 시작합니다.
-	//
-	//
-	//for (int a1 = 0; a1 < 5; a1++)
-	//{
-	//
-	//	a[a1] = (a1 + 1) * 10;
-	//
-	//	printf("a[%d] = %d\n", a1, a[a1]);
-	//
-	//
-	//	
-	//		
-	//
-	//}
-
-
-	// 배열의 메모리 공간은 프로그램이 실행하는 동안 
-	// 변경할 수 없습니다.
-
-
-	//int array[] = { 10,20,30,40,50 };
-	//
-	//int size = sizeof(array) / sizeof(array[0]);
-	//
-	//
-	//for (int i = 0; i < size; i++)
-	//{
-	//	printf("array[%d]의 값 ; %d\n", i, array[i]);
-	//
-	//
-	//}
-	//printf(" 시작주소: %p\n", array);
-	//
-	//
-	//
-	//int* pointer = array;
-	//
-	//pointer = pointer + 1;
-	//
-	//
-	//
-	//*pointer = 25;
-	//
-	// printf("pointer 변수의 값 : %p\n", pointer);
-	// printf("array[%d]의 주소 : %p\n", 1, &array[1]);
-	// 
-	// printf("array[%d]의 값 : %d\n", 1, array[1]);
-
-
-
-	// 배열의 크기는 생햑할 수 있으며. 초기화 목록에서.
-	// 설정한 요소에 따라 배열의 크기 가 결정됩니다.
-
-
-	//배열은 연속적인 메모리 공간을 가지고 있으며. 배열의 이름은
-	//배열의 시작 주소를 가리킵니다.
-
-
-#pragma region 문자열
-	// 연속적인 메모리 공간에 저장된 문자 변수의 집합입니다.
-
-	//const char* message = "packet";
-	//char title[] = { "league of legend" };
-	//
-	//
-	// //*message = 'e';
-	//
-	// printf("message가 가리키는 문자열이 뭘까요? : %s\n", message);
-	// printf("message[3]의 값 : %c\n", message[3]);
-	//
-	// message = "bit";
-	//
-	// //문자열의 경우 포인터를 이용하여 문자를 상수를 가리키도록 할 수
-	// //있으며, 문자에 상수는 데이터 영역의 읽기 전용 공간에
-	// // 저장되기 때문에 문자열의 값을 변경할 수 없습니다.
-	//
-	// printf("message의 주소 값: %p\n", message);
-	// printf("message가 가리키는 문자열이 뭘까요? : %s\n", message);
-	//
-	//
-	// // 문자열은 공백도 함께 메모리 공간에 포함되며, 마지막 메모리
-	// // 공간의 요소에 문자열 끝을 알려주는 제어 문자가 추가됩니다.
-	//
-	// printf("타이틀의 값은 ? : %s\n", title);
-	//
-	//title[6] = '\0';
-	//
-	//printf("타이틀의 값은 ? : %s\n", title);
-
-	///// title[] = { "clash of clan" };
-
-	 //문자열의 경우 서로 연속적인 메모리 공간으로 연결되어 있지만.
-	 // 문자 배열 사이에 무효의 문자를 넣게 되면 무효의 문자까지만 출력됩니다.
-#pragma endregion
-
-#pragma region (2)차원 배열
-// 배열의 요소로 또 다른 배열을 가지는 배열입니다.
-
-
-int array2D[3][3] =
+void a1()
 {
 
 
-	{ 1,2,3 },
-	{ 4,5,6 },
-	{ 7,8,9 },
-};
+	printf("a1\n");
 
-for(int i = 1; i < 4; i++ )
-	for (int a = 1; a < 4; a++)
-	{
-		printf("array2D[%d][%d]의 값: %d\n",i, a, i*a);
-	}
+
+}
+
+float a3()
+{
+	return 36.5f;
+
+
+}
+
+int a2()
+{
+	return 100;
 
 	
+
+
+}
+
+void a4(int x, int y)
+{
+	printf(" x = %d\n y = %d\n", x, y);
+}
+
+
+void a5(char grade, int height, float temperature)
+{
+	printf("등급 : %c\n", grade);
+	printf("키 : %d\n", height);
+	printf("채온 : %f\n", temperature);
 }
 
 
 
 
+void main()
+{
 
-// 2차원 배열은 행과 열로 구분되며, 앞에 있는 배열은 
-// 행을 의미하고, 뒤에있는 배열은 열을 의미합니다.
+
+
+
+	
+	
+	
+
+
+#pragma region 함수
+	// 하나의 특별한 목적의 작업을 수행하기 위해
+	// 독립적으로 설게된 코드의 집합입니다.
+
+	//a1();
+
+
+
 #pragma endregion
 
 
 
 
-//pull request
+#pragma region 반환형
 
+	// 함수가 실행을 끝내고 돌려주는 결과의 자료형입니다.
 
-
-
-
-
-#pragma endregion                                                               
-	
-	
+	// 함수의 경우 자료형과 반환하는 값의 형태가
+	// 일치하지 않으면 원하는 값을 얻을 수 없습니다.
 
 	
+
+	//printf("온도 : %f\n", a3());
+
+
+#pragma endregion
+
+#pragma region 매개 변수
+	// 함수의 정의에서 전달받은 인수를 함수 내부로
+	// 전달하기 위해 사용하는 변수입니다.
+
+
+	a4(10, 20); 
 	
+
+	// 매개 변수는 함수 내부에서만 연산이 이루어지며,
+	// 함수가 종료되면 메모리에서 해제됩니다.
+
+	//하나의 함수에 서로 다흔 자료형의 매개 변수를 함께
+	//정의할 수 있으며. 여러 개의 매개 변수를 생성할 수 있습니다.
+
+
+
+#pragma endregion
+
+
+
+	//수정
+
+
+
+
+
+
+}
